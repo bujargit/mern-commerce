f4bc3762cd8053d2e63b152baf80abedbc007904
unknown <buj4r.hyseni@gmail.com>
2023-12-15 13:57:59 +0100

    Pay now button added

--- client/src/screens/Cartscreen.js
+++ client/src/screens/Cartscreen.js
@@ -1,22 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeCartItemQuantity, deleteFromCart } from "../actions/cartActions"; 
+import { changeCartItemQuantity, deleteFromCart } from "../actions/cartActions";
 
 const Cartscreen = () => {
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.addToCartReducer);
-
   const handleQuantityChange = (itemId, newQuantity) => {
     dispatch(changeCartItemQuantity(itemId, newQuantity));
   };
 
   const handleDelete = (itemId) => {
-    dispatch(deleteFromCart({ _id: itemId })); 
+    dispatch(deleteFromCart({ _id: itemId }));
   };
 
+  let subtotal = cartItems.reduce(
+    (acc, item) => acc + item.quantity * item.price,
+    0
+  );
+
   return (
-    <div className="container">
+    <div className="container-fluid">
       <div className="row mt-5 justify-content-center">
-        <div className="col-md-8">
+        <div className="col-md-8 card">
           <h1 className="text-center m-5">My Cart</h1>
           <table className="table table-bordered">
             <thead>
@@ -36,7 +40,9 @@ const Cartscreen = () => {
                   <td>
                     <select
                       value={item.quantity}
-                      onChange={(e) => handleQuantityChange(item._id, parseInt(e.target.value))}
+                      onChange={(e) =>
+                        handleQuantityChange(item._id, parseInt(e.target.value))
+                      }
                     >
                       {[...Array(item.countInStock).keys()].map((x, i) => (
                         <option key={i} value={i + 1}>
@@ -47,12 +53,21 @@ const Cartscreen = () => {
                   </td>
                   <td>{item.quantity * item.price}</td>
                   <td>
-                    <i className="far fa-trash-alt" onClick={() => handleDelete(item._id)}></i>
+                    <i
+                      className="far fa-trash-alt"
+                      onClick={() => handleDelete(item._id)}
+                    ></i>
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
+          <hr />
+          <h2 className="text-center">SubTotal: {subtotal} &#8364;</h2>
+          <hr />
+          <div className="text-center p-3">
+            <button className="btn">PAY NOW</button>
+          </div>
         </div>
       </div>
     </div>
